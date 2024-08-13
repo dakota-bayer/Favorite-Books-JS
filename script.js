7a22c07132c14ebde23a16618212bef5683c0180
dakota-bayer <89050126+dakota-bayer@users.noreply.github.com>
2024-08-13 06:40:06 -0500

    Added books to array

---
+++ script.js
@@ -0,0 +1,38 @@
+const books = [
+  {
+    title: "Endurance: Shackleton's Incredible Voyage",
+    author: "Alfred Lansing",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/51R900QX5BL._SX294_BO1,204,203,200_.jpg",
+  },
+  {
+    title: "The Hound of the Baskervilles",
+    author: "Arthur Conan Doyle",
+    coverUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Cover_%28Hound_of_Baskervilles%2C_1902%29.jpg/571px-Cover_%28Hound_of_Baskervilles%2C_1902%29.jpg",
+  },
+  {
+    title:
+      "The Millionaire Next Door: The Surprising Secrets of America's Wealthy",
+    author: "Thomas Stanley, William Danko",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/71dpI4OARxS.jpg",
+  },
+  {
+    title: "Flags of Our Fathers",
+    author: "James Bradley",
+    coverUrl: "https://m.media-amazon.com/images/I/51yfd86hYLL.jpg",
+  },
+  {
+    title: "Around the World in Eighty Days",
+    author: "Jules Verne",
+    coverUrl:
+      "https://m.media-amazon.com/images/I/61QnzNzI9fS._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    title: "The Richest Man in Babylon",
+    author: "George S. Clason",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543897827i/43097201.jpg",
+  },
+];
